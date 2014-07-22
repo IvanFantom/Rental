2fa3e2488c6147ac40ce40d6677a29df18d075cc
Ivan Fantom <ziv78@yandex.ru>
2014-07-22 16:27:42 +0300

    adding Membership Service

---
+++ Rental.Common/IMembershipService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rental.Common
+{
+    public interface IMembershipService
+    {
+        void LogInUser(long userId);
+        void LogOutUser(long userId);
+    }
+}
---
+++ Rental.Services/MembershipService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rental.Common;
+using Rental.Interfaces;
+
+namespace Rental.Services
+{
+    public class MembershipService : BaseService, IMembershipService
+    {
+        public MembershipService(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        public void LogInUser(long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogOutUser(long userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
