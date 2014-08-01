efcae59e9bd85461567cebfc68fe7a6646481ceb
Ivan Fantom <ziv78@yandex.ru>
2014-08-01 16:17:40 +0300

    modifying filter bar and advert table layout

--- Rental.WebUI/ViewModels/Home/FilterViewModel.cs
+++ Rental.WebUI/ViewModels/Home/FilterViewModel.cs
@@ -15,22 +15,29 @@ namespace Rental.WebUI.ViewModels.Home
             AdvertType = AdvertTypeViewModel.None;
         }
 
+        [Display(Name = "Min Price")]
         [DataType(DataType.Currency, ErrorMessage = "{0} has wrong format")]
         [Range(typeof(decimal), "0", "20000", ErrorMessage = "{0} is out of range")]
+        [LessThanOrEqualTo("MaxPrice", ErrorMessage = "{0} should be less than or equal to {1}")]
         public decimal MinPrice { get; set; }
 
+        [Display(Name = "Max Price")]
         [DataType(DataType.Currency, ErrorMessage = "{0} has wrong format")]
         [Range(typeof(decimal), "0", "20000", ErrorMessage = "{0} is out of range")]
-        [GreaterThanOrEqualTo("MinPrice", ErrorMessage = "MinPrice should be <= MaxPrice")]
+        [GreaterThanOrEqualTo("MinPrice", ErrorMessage = "{0} should be greater than or equal to {1}")]
         public decimal MaxPrice { get; set; }
 
+        [Display(Name = "Min Footage")]
         [Range(0, 20000, ErrorMessage = "{0} is out of range")]
+        [LessThanOrEqualTo("MaxFootage", ErrorMessage = "{0} should be greater than or equal to {1}")]
         public int MinFootage { get; set; }
 
+        [Display(Name = "Max Footage")]
         [Range(0, 20000, ErrorMessage = "{0} is out of range")]
-        [GreaterThanOrEqualTo("MinFootage", ErrorMessage = "MinFootage should be <= MaxFootage")]
+        [GreaterThanOrEqualTo("MinFootage", ErrorMessage = "{0} should be greater than or equal to {1}")]
         public int MaxFootage { get; set; }
 
+        [Display(Name = "Type")]
         public AdvertTypeViewModel AdvertType { get; set; }
     }
 }
